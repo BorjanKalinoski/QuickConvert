94e2c8924be28505d74d12ee83a5fafa5268c5fc
Borjan kalinoski <boki_borjan@hotmail.com>
2019-10-05 21:32:01 +0200

    Revert "kur commit"
    This reverts commit 836988c2

--- routes/images.js
+++ routes/images.js
@@ -6,18 +6,13 @@ const upload = multer();
 
 router.post('/download', upload.array('files', process.env.MAX_COUNT), validateFiles, convertFiles, zipFiles, async (req, res) => {
     res.status(200);
-    // res.set({
-    //     'Cache-Control': 'no-cache',
-    //     'Content-Type': 'application/zip',
-    //     'Content-Length': 1,
-    //     'Content-Disposition': 'attachment; filename=' + 'files.zip'
-    // });
-
-
-
+    res.set({
+        'Cache-Control': 'no-cache',
+        'Content-Type': 'application/zip',
+        'Content-Length': 1,
+        'Content-Disposition': 'attachment; filename=' + 'files.zip'
+    });
     return res.send(req.buffer);
 });
 
-module.exports = {
-    router: router
-};
+module.exports = router;
