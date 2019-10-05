2f4355a8382fd61d84b2a53f15818410348900aa
Borjan kalinoski <boki_borjan@hotmail.com>
2019-10-05 15:15:44 +0200

    Configure webpack

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
+    target: 'web',
     entry: './src/index.tsx',
     output: {
         path: path.resolve(__dirname, 'build/'),
